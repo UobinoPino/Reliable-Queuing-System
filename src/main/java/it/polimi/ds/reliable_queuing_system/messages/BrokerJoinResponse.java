a2fd7065e8d3123b31b698ea098a8a2adfbbab4d
LeonardoMantovani <leonardo@mantovani.us>
2025-03-01 12:12:07 +0100

    Removed json maven dependency + updated `BrokerJoinResponse` message to also provide the new broker with the current system `SharedState`.

--- src/main/java/it/polimi/ds/reliable_queuing_system/messages/BrokerJoinResponse.java
+++ src/main/java/it/polimi/ds/reliable_queuing_system/messages/BrokerJoinResponse.java
@@ -1,9 +1,11 @@
 package it.polimi.ds.reliable_queuing_system.messages;
 
+import it.polimi.ds.reliable_queuing_system.broker.SharedState;
+
 /// Message sent by the leader to the newly added broker (eventually through the follower it
 /// originally contacted) to confirm the entrance in the system and provide the current
 /// replicated state.
 public record BrokerJoinResponse(
-        //TODO: add a parameter for each variable of the replicated state
+        SharedState sharedState
 ) implements Message {
 }
